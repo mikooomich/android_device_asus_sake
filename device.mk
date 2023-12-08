8b3de1192ea6f6c26e1bbac0b76b0979c84ede2b
mikooomich <mikooomichael@gmail.com>
2023-12-08 14:32:29 -0500

    Add personal build features
    Change-Id: I87cb25f6f06c74451ce386a001fbfe3c12f94047

--- device.mk
+++ device.mk
@@ -488,4 +488,18 @@ PRODUCT_PACKAGES += \
     libwpa_client \
     wpa_supplicant \
     wpa_supplicant.conf
-    
+
+# Inline Gapps
+include vendor/gapps/arm64/arm64-vendor.mk
+
+# I hate Safety Net
+PRODUCT_PACKAGES += \
+    ih8sn
+
+DEVICE := $(subst lineage_,,$(TARGET_PRODUCT))
+IH8SN_CONFIG_PATH := $(LOCAL_PATH)/prebuilt/etc/ih8sn_$(DEVICE).conf
+
+ifneq ($(wildcard $(IH8SN_CONFIG_PATH)),)
+PRODUCT_COPY_FILES += \
+    $(IH8SN_CONFIG_PATH):/system/etc/ih8sn.conf
+endif
